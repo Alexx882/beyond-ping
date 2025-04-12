bfc82558c005321705d79fcd40e83512d8f58801
Christof Bachmann <christof.bachmann@gmail.com>
2025-04-12 21:38:41 +0200

    Wip: SoundManager + Sounds

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+
+public enum SoundType
+{
+    THRUSTER,
+    PICKUP,
+    CRASH,
+    LOST
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1f)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}

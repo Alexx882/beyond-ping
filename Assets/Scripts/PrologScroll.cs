dd8ddbbb3c9a32ee3eb8091c71abc34dc6cb63a2
Alexander Lercher <alexander.lercher@aau.at>
2025-04-13 11:21:26 +0200

    Prolog skip and connection lost gizmo circle

--- Assets/Scripts/PrologScroll.cs
+++ Assets/Scripts/PrologScroll.cs
@@ -7,18 +7,25 @@ public class PrologScroll : MonoBehaviour
     public Transform textTransform;
 
     public float scrollSpeed = 2f;
+    public float scrollSpeedSkip = 15f;
 
     // public float endYPosition = 1000f;
     public float endY = 17.5f;
     public string nextSceneName = "Level1"; // Replace with your scene name
 
     private bool animating = true;
-
+    
     void Update()
     {
         if (animating)
         {
-            textTransform.position += new Vector3(0f, 0.5f, 0.5f) * (scrollSpeed * Time.deltaTime);
+            float currentScrollSpeed = scrollSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                currentScrollSpeed = scrollSpeedSkip;
+            }
+
+            textTransform.position += new Vector3(0f, 0.5f, 0.5f) * (currentScrollSpeed * Time.deltaTime);
             if (textTransform.position.y > endY)
             {
                 animating = false;
